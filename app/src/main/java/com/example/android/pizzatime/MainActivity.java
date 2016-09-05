3981536a7f3d4e76af46857a8c608a312b9414c5
Shawn Begier <Shawn Begier>
2016-09-05 16:42:30 -0400

    Adding code behind

--- app/src/main/java/com/example/android/pizzatime/MainActivity.java
+++ app/src/main/java/com/example/android/pizzatime/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.pizzatime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    int pizzaCount = 0;
+    protected void addPizza(View view) {
+        TextView counter = (TextView)findViewById(R.id.counter);
+        pizzaCount += 1;
+        counter.setText("" + pizzaCount);
+
     }
 }
