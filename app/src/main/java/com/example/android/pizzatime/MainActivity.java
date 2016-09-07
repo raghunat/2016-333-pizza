ab7c70fc4a1da878b278a76b6a13ca14d123eaa2
Nicholas Horst Hannmann <hann4024@fredonia.edu>
2016-09-07 14:51:22 -0400

    Code updates from 9/7

--- app/src/main/java/com/example/android/pizzatime/MainActivity.java
+++ app/src/main/java/com/example/android/pizzatime/MainActivity.java
@@ -8,6 +8,12 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 private int pizzaCount = 0;
 
+    private int cheeseCount = 0;
+    private int pepperoniCount = 0;
+    private int sausageCount = 0;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +24,31 @@ private int pizzaCount = 0;
 
         pizzaCount++;
         counter.setText("" + pizzaCount);
+    }
+    protected void addCheese(View view){
+        TextView counter = (TextView)findViewById(R.id.cheese_counter);
+        cheeseCount++;
+        counter.setText("" + cheeseCount);
+    }
+
+    protected void addPepperoni(View view){
+        TextView counter = (TextView)findViewById(R.id.pepperoni_counter);
+        pepperoniCount++;
+        counter.setText("" + pepperoniCount);
+    }
+
+    protected void addsausage(View view){
+        TextView counter = (TextView)findViewById(R.id.sausage_counter);
+        sausageCount++;
+        counter.setText("" + sausageCount);
+    }
+
+    protected void calculateOrder(View view){
+        int cheesePrice = 5;
+        int pepperoniPice = 6;
+        int sausagePrice = 7;
+
+        TextView totalTextView = (TextView)findViewById(R.id.order_total);
+        totalTextView.setText("" + total);
     }
 }
