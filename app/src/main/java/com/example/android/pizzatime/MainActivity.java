80d5845a3e5ba7eb7eb079bc18bb31f51ee1c609
Shawn Begier <Shawn Begier>
2016-09-07 14:51:39 -0400

    Code update from 9/7

--- app/src/main/java/com/example/android/pizzatime/MainActivity.java
+++ app/src/main/java/com/example/android/pizzatime/MainActivity.java
@@ -12,11 +12,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    int pizzaCount = 0;
+    /*private int pizzaCount = 0;
     protected void addPizza(View view) {
         TextView counter = (TextView)findViewById(R.id.counter);
         pizzaCount += 1;
         counter.setText("" + pizzaCount);
+    }
+    */
+    private int pepperoniCount = 0;
+    protected void addPepperoni(View view) {
+        TextView counter = (TextView)findViewById(R.id.pepperoni_counter);
+        pepperoniCount += 1;
+        counter.setText("" + pepperoniCount);
 
+    }
+    private int mushroomCount = 0;
+    protected void addMushroom(View view) {
+        TextView counter = (TextView)findViewById(R.id.mushroom_counter);
+        mushroomCount += 1;
+        counter.setText("" + mushroomCount);
+
+    }
+    private int supremeCount = 0;
+    protected void addSupreme(View view) {
+        TextView counter = (TextView)findViewById(R.id.supreme_counter);
+        supremeCount += 1;
+        counter.setText("" + supremeCount);
+
+    }
+
+    protected void calculateOrder(View view){
+        int pepperoniPrice = 6;
+        int mushroomPrice = 7;
+        int supremePrice = 8;
+
+        int total = pepperoniPrice * pepperoniCount;
+        total += mushroomPrice * mushroomCount;
+        total += supremePrice * supremeCount;
+
+        TextView totalTextView = (TextView)findViewById(R.id.order_total);
+        totalTextView.setText("" + total);
     }
 }
