255bcbab9eca10dfd44c3a64d199eb664e8db7a9
Sam McCagg <Sam@Owners-MacBook-Pro.local>
2016-09-07 14:50:22 -0400

    9.7.2016 class

--- app/src/main/java/com/example/android/pizzatime/MainActivity.java
+++ app/src/main/java/com/example/android/pizzatime/MainActivity.java
@@ -8,7 +8,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int pizzaCount = 0;
+//    private int pizzaCount = 0;
+    private int cheeseCount = 0;
+    private int pepperoniCount = 0;
+    private int sausageCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +19,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    protected void addPizza(View view) {
-        TextView counter = (TextView)findViewById(R.id.counter);
-        pizzaCount += 1;
-        counter.setText("" + pizzaCount);
+//    protected void addPizza(View view) {
+//        TextView counter = (TextView)findViewById(R.id.counter);
+//        pizzaCount += 1;
+//        counter.setText("" + pizzaCount);
+//    }
+
+    protected void addCheese(View view) {
+        TextView counter = (TextView)findViewById(R.id.cheese_counter);
+        cheeseCount += 1;
+        counter.setText("" +  cheeseCount);
+
+    }
+    protected void addPepperoni(View view) {
+        TextView counter = (TextView)findViewById(R.id.pepperoni_counter);
+        pepperoniCount += 1;
+        counter.setText("" +  pepperoniCount);
+
+    }
+    protected void addSausage(View view) {
+        TextView counter = (TextView)findViewById(R.id.sausage_counter);
+        sausageCount += 1;
+        counter.setText("" +  sausageCount);
+    }
+    protected void calculateOrder(View view) {
+        int cheesePrice = 5;
+        int pepperoniPrice = 6;
+        int sausagePrice = 7;
+        int total = cheesePrice * cheeseCount + pepperoniPrice * pepperoniCount + sausagePrice * sausageCount;
+
+        TextView totalTextView = (TextView)findViewById(R.id.order_total);
+        totalTextView.setText("$" +total);
     }
 }
