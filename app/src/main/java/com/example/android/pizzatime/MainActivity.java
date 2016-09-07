9f8a9e4e62cb01c56e22dea87987877ba5b5aaaa
Professor Raghunath <raghunat@fredonia.edu>
2016-09-07 14:51:16 -0400

    Code updates from 9/7

--- app/src/main/java/com/example/android/pizzatime/MainActivity.java
+++ app/src/main/java/com/example/android/pizzatime/MainActivity.java
@@ -9,6 +9,10 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private int pizzaCount = 0;
+    private int cheeseCount = 0;
+    private int pepperoniCount = 0;
+    private int soyCount = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +21,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void addPizza(View view) {
-        TextView counter = (TextView)findViewById(R.id.counter);
-        pizzaCount += 1;
-        counter.setText("" + pizzaCount);
+//        TextView counter = (TextView)findViewById(R.id.counter);
+//        pizzaCount += 1;
+//        counter.setText("" + pizzaCount);
+    }
+
+    protected void addCheese(View view) {
+        TextView counter = (TextView)findViewById(R.id.cheese_counter);
+        cheeseCount += 1;
+        counter.setText("" + cheeseCount);
+    }
+    protected void addPepperoni(View view) {
+        TextView counter = (TextView)findViewById(R.id.pepperoni_counter);
+        pepperoniCount += 1;
+        counter.setText("" + pepperoniCount);
+    }
+    protected void addSoy(View view) {
+        TextView counter = (TextView)findViewById(R.id.soy_counter);
+        soyCount += 1;
+        counter.setText("" + soyCount);
+    }
+
+    protected void calculateOrder(View view) {
+        int cheesePrice = 5;
+        int pepperoniPrice = 6;
+        int soyPrice = 7;
+
+        int total = cheesePrice * cheeseCount;
+        total += pepperoniPrice * pepperoniCount;
+        total += soyPrice * soyCount;
+
+        TextView totalTextView = (TextView)findViewById(R.id.order_total);
+        totalTextView.setText("" + total);
     }
 }
